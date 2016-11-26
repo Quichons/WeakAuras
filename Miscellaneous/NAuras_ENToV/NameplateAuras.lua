f227cdbd5d9ce9e51d945ee7c0e5df508574f1dd
Aethys256 <aethys256@users.noreply.github.com>
2016-11-26 15:26:13 +0100

    NameplateAuras

---
+++ Miscellaneous/NAuras_ENToV/NameplateAuras.lua
@@ -0,0 +1,127 @@
+
+NameplateAurasDB = {
+}
+NameplateAurasAceDB = {
+	["profileKeys"] = {
+		["Aèthÿs - Elune"] = "EN & ToV",
+	},
+	["profiles"] = {
+		["EN & ToV"] = {
+			["HideBlizzardFrames"] = false,
+			["DefaultSpellsAreImported"] = true,
+			["StacksTextAnchorIcon"] = "BOTTOMRIGHT",
+			["CustomSpells2"] = {
+				--- Emerald Nightmare
+				-- Nythendra
+				[203097] = { -- Rot
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 203097,
+				},
+				[204470] = { -- Volatile Rot
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 204470,
+				},
+				--- Trial of Valor
+				-- Guarm
+				[227539] = { -- Fiery Phlegm
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 227539,
+				},
+				[227566] = { -- Salty Spittle
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 227566,
+				},
+				[227570] = { -- Dark Discharge
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 227570,
+				},
+				[228744] = { -- Flaming Volatile Foam
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 228744,
+				},
+				[228810] = { -- Briney Volatile Foam
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 228810,
+				},
+				[228818] = { -- Shadowy Volatile Foam
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 228818,
+				},
+				-- Helya
+				[193367] = { -- Fetid Rot
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 193367,
+				},
+				[227903] = { -- Orb of Corruption
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 227903,
+				},
+				[228054] = { -- Taint of the Sea
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 228054,
+				},
+				[228058] = { -- Orb of Corrosion
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 228058,
+				},
+				[232450] = { -- Corrupted Axion
+					["enabledState"] = "all",
+					["auraType"] = "debuff",
+					["iconSize"] = 45,
+					["showOnEnemies"] = false,
+					["showOnFriends"] = true,
+					["spellID"] = 232450,
+				}
+			},
+			["TimerTextAnchorIcon"] = "CENTER",
+		},
+	},
+}
---
+++ Miscellaneous/NAuras_ENToV/README.txt
@@ -0,0 +1,7 @@
+- Be on Character Selection
+- Copy/Paste 'NameplateAuras.lua' to 'C:\Program Files (x86)\World of Warcraft\WTF\Account\YOUR_ACCOUNT\SavedVariables'
+- Open NameplateAuras.lua with a Text Editor, then edit line 6 :
+	["Aèthÿs - Elune"] = "EN & ToV",
+	to
+	["YourName - YourServer"] = "EN & ToV",
+- Save, Close the Text Editor, Log-in and ... voilà.
