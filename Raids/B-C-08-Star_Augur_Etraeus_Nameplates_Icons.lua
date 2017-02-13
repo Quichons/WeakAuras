bed86225a472354f4a47867e3f44b2645980a832
Aethys256 <aethys256@users.noreply.github.com>
2017-02-13 18:05:49 +0100

    [StarAugur] Nameplate WA

---
+++ Raids/B-C-08-Star_Augur_Etraeus_Nameplates_Icons.lua
@@ -0,0 +1,150 @@
+--- WeakAura Strings ---
+dG1TAaqEPsTELQ49iL08rkkUpsj2pmGKIelsk9iKIsBcPi0NqksAuOWPqrVskQQMLaUfsHDHunmbYXqqlJc6zkvAAuGUMsfBtkkFtkQmoHkXINIQ06Kk5DuO7jO2hsrWdROOCqPQ0crk1dfOMOqfxuQOnQe9reWifQKoPuKvIQEjsr1mPOWnfQANsv(PuvnuPchfPi1sPOQNQOPQKUkc0wLQIVsrr15eQuNzkQI1QuvElfvMlff5UuuAVI6VuKbtoSklMc4XiXKvkxMQnJK(Sqz0kHtlYQrkI8ALQ0SrPBlKDRQFRWWfKtKue1YP0ZrLPl56iA7i03vQQgVu4utrv5mH518mkp3YZT8AEszWoS51CpcZlJlnxCVJHnBhc3XGnZW42G7gxYuPXo7MR8ClXfI96E1Bu5zEsHKRgrREJkpbp4bFEEuQerF1MIfZTXp28CVGYZJsLgpxEnp5QZw518CByIcjxnIY0opPqYvJO88iTNj)js3j62UxAViNbtJ5kptFQYR552WefsUAeLPDEsHKRgr5j7GYXcLOm(zApouyiATzbfaQzbrlgb)rApt(tK(FrEi2R8MOmEoOWq0k4HGGGmxlbSU)TMfkmKH72SDI7DcaEiiiiZ1AEFR1SqHHmC3M1mcne80IrWFK2ZK)ePZLI9CZqollhuyiAf8qqqqMJbTsaVMHCw2MEQdggg0og0W4MjTW0CeAwZcfgQLW2aGhcccYCmOvc41mKZY20tDWWWG2XGgChM0ctZrOznluyOwdBdaEiiiiZXGwjGxZqolBtp1bddjSz7AWDyslmnhHM1SqHHA3Tna4HGGGmhdALaEnd5SSn9uhmmKWMTRbnitAHP5i0SMfkmuRbBbpTye8G)iTNj)jsNYcNYE7DrEi2R8Pxv6JXbfgIk5tPsJNbtWdbbbrdAaTCyP4q)f5HyVY5G48QsFmoWdbbbXoOCSqXVVvOWqeWR4tFk6cTVX7REJIbtJGhccckrfk(9TcfomuBNLB)JfQFdW5wIxnSD3cEiiiOHfk(9TcfomuB)gGZTeVAKSoKzgn5YTVZ2cEiiiOHfk(9TcfomuB)gGZTeVAKSoKzwZx523zBbpeee0Wcf)(wHchgQTFdW5wIxnswhQZYTVZ2cEiiiOHfk(9TcfomuB)gGZTeVAKSoKxU9D2wOAH)qGhccccccckrf6hf0rApt(tKo3r03GQf(dEiiiiiiiiiiiOJ0EM8Ni9)I8qSx5XpQ04Nhrhkmeb8k4(CwgT)f5HyVYJFuPX3Y0i4HGGGGGGGGGGGos7zYFI0)lYdXELpxm2)sBY)nU5r0HcdraVcUpNLr7FrEi2R85IX(xAt(VX1Y0i4HGGGGGGGGGGGos7zYFI0)lYdXELh)1iojU6Ekouyic4vW95SmA)lYdXELh)1iojU6EkEltJGhcccccccccccA6vW95SmA)lYdXELh)OsJVnaeHmncEiiiiiiiiiiiOPxb3NZYO9Vipe7v(CXy)lTj)34AdaritJGhcccccccccccA6vW95SmA)lYdXELh)1iojU6EkEBaicdkiMgbpeeeeeeeeeee0rApt(tKo3r03Gcdvws3i4HGGGGGGG8Fd8qqqqqqqq0GgqM35QH1Hg2uS0FSqCEvPpgh4HGGGCwoh8qqqqqqqqjQqhP9m5pr6ChrFdQw4p4HGGGGGGGGGGGMEfCFolJ2)I8qSx5XpQ04BdaDK2ZK)eP)xKhI9kp(rLg)8i6qdleHmncEiiiiiiiiiiiOPxb3NZYO9Vipe7v(CXy)lTj)34AdaDK2ZK)eP)xKhI9kFUyS)L2K)BCZJOdnSqbX0i4HGGGGGGGGGGGMEfCFolJ2)I8qSx5XFnItIRUNI3ga6iTNj)js)Vipe7vE8xJ4K4Q7P4qdleHbfetJGhccccccccccc6iTNj)js)Vipe7vE8Jkn(5r0Hcd9jwJGhccccccccccc6iTNj)js)Vipe7v(CXy)lTj)34Mhrhkm0NyncEiiiiiiiiiiiOJ0EM8Ni9)I8qSx5XFnItIRUNIdfg6tSgbpeeeeeeeeeee0rApt(tKo3r03Gcd9jwJGhcccccccY)niWdbbb5)g49Fd8G)iTNj)jsV8gfP17ymyHcdrR0IrWFK2ZK)ePxEJI06n9BEdOWqbze8G)iTNj)jspMxREJI06lTJOogdwOWqujFkvA8mycEiiiOJ0EM8Ni9YBuKwVPFZBafg6iTNj)jsV8gfP1B638gqn)qeAe8qqqqjQqhP9m5pr6L3OiTEt)M3aIMaen7rApt(tKE5nksR3XyWcvl8h8qqqqqqqqSdkhlev7f5qHHc26x5lTxKZODP9I8wMgbpeeeeeeeev7f5Dn9AP9I8zzV6y0s7(gmnNaMVFc2loTmncEiiiiiiiiQ2lY7A61s7f5MHt0zzeetJGhcccccccIQ9I8UMEnt7XzKOm(zApU5i0SbGsug)mTh3CgAwMgbpeeeeeeeev7f5Dn9QJr6lgTb3zV1onFBaOGcafetJGhcccccccIQ9I8UIRPnNbtJGhcccccccc8qqqqqqqquTxKtprz8qHHOAViVRGT(v(Q3OiToJpXgaQL29nyAobmF)eSxCAda9j2aq0iUW0i4HGGGGGGGOAViNEIY47A6vFzz7yK(IJbtJGhcccccccIQ9IC6jkJVRPx9XTL3i4b7WYGWaqegaIWaqeY0i4HGGGGGGGapeeeeeeeev7f505kB6JziuyiQ2lY7kyRFLVC81SSPpgJpXgaQL29nyAobmF)eSxCAda9j2aq0O5yAe8qqqqqqqquTxKtNRSPpMHDn9A54lgTlhFXzMmtlnFt7JME1Kj6RRlBdarObda12pbxnJM61zltJGhcccccccIQ9IC6CLn9XmSRPxDmsFXOL29VU2F8MrNlxBdar1ErEaOwA3)6A)XBgDUCTnauqbGcIPrWdbbbbbbbbEiiiiiiiiQ2lYPZv20hdkmev7f5DfS1VYxo(Aw20hJXNyda1s7(gmnNaMVFc2loTbG(eBaiA0CmncEiiiiiiiiQ2lYPZv20hRRPxlhFXOD54loZKzAP5BAF00RMmrFDDzBaiddkauB)eC1mAQxNTmncEiiiiiiiiQ2lYPZv20hRRPxDmsFXODT)omJoxU2gaIQ9I8aqTR93Hz05Y12aqbfakiMgbpeeeeeeee4HGGGGGGGos7zYFI0lVrrA9ogdwZDK2ZK)ePxEJI06n9BEdZcfgIQ9ICJGhcccY)nWdbbbz9I0(qhP9m5pr6L3OiTEhJbR5os7zYFI0lVrrA9M(nVHzncE)3ap4ps7zYFI0VTTvVrrA91rVlYdXELdfgIk5tPsJNb5NQaqjkJpau5nQaqL3OmKj4HGGGyhuowO)I8qSx5qHHc2uVlY7G9kNob8Q3f5DWELVCyj4NkgKFQyAe8qqqqjQq)OG(lYdXELdvl8hY6fP9nc5)g4HGGGyhuowiQ2lYHcdDK2ZK)ePhZRvVrrA9L2ruhJbldMgbpeeeev7f5DfmRF2(YY2Xi9fhdMgbpeeeev7f5Dn9QJr6lgTb3zV1onFBaO)I8qSx5bGAdUZERDA(2aqbfaIgggetJGhcccIQ9IC6jkJVRPxREJI06msugptJGhcccIQ9IC6CLn9X6A61Q3OyuEJIPrWdbbbr1EroDUYM(yg210RvVrXO8gLHmncEiiiiQ2lY7AUySFgmncE)3ap4ps7zYFI0DIUT7L2lYHcdrL8PuPXZGj4HGGGOoSqPWegaIM9iTNj)jsV8gfP17ymyH2gGhccccccc6iTNj)jsV8gfP17ymynxYSDfxtBodMgbpeeeK)BGhccc6iTNj)jsV8gfP1B638gqHHcYi49Fd8G)iTNj)jspMxZuSF5WsWpvqHHOs(uQ04zq(PIj4HGGGOoSqCPyFaOYBuqPhk0LSCmos7zYFI05sXEUziNLLJj02a8qqqqqqqqjQqe8tvCCdqsLkdYpvbG4sXEMq1c)HSErAFOYBugH8Fdc8qqqq(VbEiiiiRxK2h6tSgbV)B5kx5j1XxlzVyC5z)eC1mAQxN5jNZsTJr6R8m4o7T2P5ZZYMIfZT518S6oKNNui5QruEsHKRgrMQUd55jxDfjxE6e9VYZGxCfu8oh3fZZtso3e5NQmTZtkKC1ikpPs(uQ04zWe8qqqq0GgqDyDI(xqwYhkrfYadooOe3ttTGo7G83apeeeuIkeb)u17ICgTgyWXryltiqZegQDwNfQVKXiTqDw2Zj5AHgwic(PQ3f5mAnWGJJWwMqGMjmuBNL9CsUaqSnV0uAEoYyKwhIRCw2lz9wOAH)qwViTVri)3apeeee4HGGGObnGcEHtzVq9Uipe7voh00Rk9X4GOjczENZlOL2lY5apeee0rApt(tKoLfoL927I8qSx5tVQ0hJJbtJGhccc6iTNj)js3j62UxAViNbtJGhcccc8qqqq0Ggqtk3dXLI9CGhcccIDq5yHIsVdfgkMxZuSF5WsWpvmAdXED72wMgbpeeeuIkuu6DOAH)GhcccccccIDq5yHi)ufaQw4jlNrWdbbbbbbbrDyHsHjmaeb8QhzebSdYqX7rgWTCmycTnapeeeeeeeeeeee5NkOWqT2lT1sNEn4kB6JXiX0i4HGGGGGGGGGGGsuH(rbrWpvnXrWpvmi)ufaQne71TBBzcvl8h8qqqqqqqqqqqqqqqq1cpz5GcdDK2ZK)ePhZRzk2VCyj4NkgKFQyAe8qqqqqqqqqqqqqqqqjQq1cpz5Gchgkk9ouTWFWdbbbbbbbbbbbbbbbbbbbDK2ZK)ePFBBREJI06RJExKhI9kNb5NQaqhP9m5pr6)f5HyVYBIY45OtaR7FaOAHNSCbGi4NQExKZG8tftMgbpeeeeeeeeeeeeeeeKZY5jQq1cpz5GQf(dEiiiiiiiiiiiiiiiiiiiOJ0EM8Ni9BBB1BuKwFD07I8qSx5mi)ufa6iTNj)js)Vipe7vEtugphDZ7BbGQfEYYfaIGFQ6DrodYpvmzAe8qqqqqqqqqqqqqqqq(VbEiiiiiiiiiiii)3apeeeeeeeK)BGhcccY)niiiiW7)wEszHtzV5jzOTR88K8DI(x55rwJ8KcjxnImTiT55zLI8TCLNCvIYE7ooK2RsJpBG8CHNITOYln4obTBZ2DxdUtqgUBZf37yyMkn2XW8KDCB518Sow)R8AEgrYwP8AUYvEsY5MENOBZgip3Z498CDHd1lfBrTyWULN2bRNxZZis2kLxZvUYtso3K)ugKF520w2a55wIk1efs2Q78AEYL(ySEEg1DTirjpJizRuEnpPS4yz9pMNPDUYvEApkEEnpJizRuEnx5kpPEuQ04ZR5zejBLYR5kx5ziIHQJ1)kVMNrKSvkVMRCLNKCUP9mEpt78KKZnTLOsnrHKT6ot78KKZnXfYzz37p2)NnqEYL2JNxZtU0hJ1ZZqNT6opJizRuEnpdD2Q7mTZvUYvEsD81mThp3R5mmp3sCHyVUN1rzVC5juEA9yPXV2DipplVrLN3NIUkn(8AEgDPpVMNHSoNx55XgAX1bjlNNRCLNjkJpt788iRrM25jxDu2lxWJVU0h5FLNn1B2uC6mp3ZyeL7fuEEBBPkn(J1uztXI5wU8AUYZ0wEAM3xt91RfDZzAwNfQVKXiTqDw2Zj5GwAt(VX2nuVlYdXELZLNpz0QnflMB5Y9impj)YMIfZT518KcjxnIYtQKpLknEgmbpeeeK1ls7dvws3i49Flx55MZDfP9kpBG8m9eDlNNnqEUL4Ojr(uQerppVV78C)PTArEPb3jODB2U7AWDcYWDBU4EhdZuPXogMNBjUqSx3nrz85EeUBZmC3DYtU6OSxUogPVYtA3)6A)XB(MiqCDnplVrf8GDyZR5EeM7ryUNH5EeM7TBUhH5Egm3JWCLR8KlpnK(U0dIEq5jXCpcnOHeMRCg
+
+--[[
+Author : Aethys, Serenity
+Version : 1.0 (02.13.2017)
+Readme :
+	WeakAura to mark friendlies nameplates in green or red according to the mark they got.
+]]--
+
+
+--- Actions\On Init\Custom ---
+
+-- Init
+local iconSize = {60, 60};
+aura_env.nameplateIcons = {
+    ["Green"] = 236595,
+    ["Red"] = 236612
+};
+aura_env.signsSpells = {
+    [({GetSpellInfo(205429)})[1]] = "1",
+    [({GetSpellInfo(205445)})[1]] = "2",
+    [({GetSpellInfo(216345)})[1]] = "3",
+    [({GetSpellInfo(216344)})[1]] = "4"
+};
+
+aura_env.checkNameplateSettings = function()
+    -- Force nameplates settings
+    local MZT = GetMinimapZoneText();
+    if MZT == "Eternal Observatory"
+        or MZT == "Observatoire Éternel"
+        or MZT == "Observatoire éternel"
+        or MZT == "Observatoire Eternel"
+        or MZT == "Observatoire eternel" then 
+        if not aura_env.saved then
+            aura_env.nameplateMotionSave = GetCVar("nameplateMotion");
+            aura_env.nameplateShowFriendsSave = GetCVar("nameplateShowFriends");
+            aura_env.nameplateMaxDistance = GetCVar("nameplateMaxDistance");
+            SetCVar("nameplateMotion", 1);
+            SetCVar("nameplateShowFriends", 1);
+            SetCVar("nameplateMaxDistance", 100);
+            aura_env.saved = true;
+        end
+    -- Restore original settings
+    else
+        if aura_env.saved then
+            SetCVar("nameplateMotion", aura_env.nameplateMotionSave or 1);
+            SetCVar("nameplateShowFriends", aura_env.nameplateShowFriendsSave or 0);
+            SetCVar("nameplateMaxDistance", aura_env.nameplateMaxDistance or 100);
+            aura_env.nameplateMotionSave = nil;
+            aura_env.nameplateShowFriendsSave = nil;
+            aura_env.nameplateMaxDistance = nil;
+            aura_env.saved = nil;
+        end 
+    end
+end
+
+aura_env.texturePool = {};
+aura_env.textureIndex = 0;
+
+aura_env.getTextureFromPool = function()
+    aura_env.textureIndex = aura_env.textureIndex + 1;
+    if aura_env.textureIndex > #aura_env.texturePool then
+        local frame = CreateFrame("Frame");
+        frame:SetFrameStrata("BACKGROUND");
+        frame:SetFrameLevel(0);
+        frame:SetSize(iconSize[1], iconSize[2]);
+        frame:SetPoint("CENTER", 0, 0);
+        frame:Hide();
+
+        frame.icon = frame:CreateTexture(nil, "BACKGROUND", nil, -8);
+        frame.icon:SetAllPoints();
+        frame.icon:SetVertexColor(1, 1, 1, 1);
+
+        frame.string2 = frame:CreateFontString(nil, "BACKGROUND", nil, -7);
+        frame.string2:SetFont("Fonts\\FRIZQT__.TTF", 14, "OUTLINE");
+        frame.string2:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 0, 0);
+
+        frame.string = frame:CreateFontString(nil, "BACKGROUND", nil, -7);
+        frame.string:SetFont("Fonts\\FRIZQT__.TTF", 20, "OUTLINE");
+        frame.string:SetPoint("TOPLEFT", frame, "TOPLEFT", 0, 0);
+
+        aura_env.texturePool[aura_env.textureIndex] = frame;
+    end
+    return aura_env.texturePool[aura_env.textureIndex];
+end
+
+aura_env.addTextureToNameplate = function(unit, icon, text, text2)
+    local nameplate = C_NamePlate.GetNamePlateForUnit(unit);
+    if not nameplate then return; end
+    local frame = aura_env.getTextureFromPool();
+    frame:ClearAllPoints();
+    frame:SetPoint("CENTER", nameplate, "CENTER", 0, -20);
+    frame.icon:SetTexture(icon);
+    frame.string:SetText(text);
+    frame.string2:SetText(text2);
+    frame:Show();
+end
+
+aura_env.everyFrame = function()
+    for i=1, #aura_env.texturePool do
+        aura_env.texturePool[i]:Hide();
+    end
+    aura_env.textureIndex = 0;
+end
+
+aura_env.getSignForUnit = function(unit)
+    for sign, text in pairs(aura_env.signsSpells) do
+        if UnitDebuff(unit, sign) then return text; end 
+    end
+    return nil;
+end
+
+-- Show (Custom)
+aura_env.everyFrame();
+
+
+--- Trigger\Custom Trigger ---
+
+-- Trigger [Status\Every Frame]
+function()
+    -- Prevent run if boss isn't around
+    if UnitName("boss1") ~= "Star Augur Etraeus" or UnitName("boss1") ~= "Etraeus, l’augure stellaire" then return; end
+
+    -- Check Nameplates Settings & Reset Frames
+    aura_env.checkNameplateSettings();
+    aura_env.everyFrame();
+
+    -- Scan signs
+    local mine = getSignForUnit("player");
+    if mine then
+        local unit, theirs;
+        for i=1, GetNumGroupMembers() do
+            unit = "raid"..tostring(i);
+            if not UnitIsUnit(unit, "player") then
+                theirs = aura_env.getSignForUnit(unit);
+                if theirs == mine then
+                    aura_env.addTextureToNameplate(unit, aura_env.nameplateIcons.Green, theirs, UnitName(unit));
+                elseif theirs then
+                    aura_env.addTextureToNameplate(unit, aura_env.nameplateIcons.Red, theirs, UnitName(unit));
+                end
+            end
+        end
+    end    
+end
+
+-- Untrigger
+function()
+    return true;
+end
