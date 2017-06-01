9845655eecdf48613e4d9fd65a84d2bb5a499c65
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2017-06-01 14:07:55 +0200

    Trilliax scrubber marker with ID

---
+++ Raids/B-C-03-Trilliax_Scrubber_Marker.lua
@@ -0,0 +1,86 @@
+--- WeakAura Strings ---
+dOemnaGibYKKQQ4sufPrrHoff8kQc1SOk1TKkQDjLmmsLogPILjvKNrvetdqCnsrTnaPVrc14OkQZrcSoPQQ6DaQ5rcc3JuAFKcLdscIwivbtKuixKesBKQeFKuKtkvLvcLzsvi3uQ0oPkPFscQgkjelvQQ8uHMkjDvkQTsckVvG6Usf2lO)srgm6WuAXsv6XamzQQllAZc4ZKOrlfNMWQjbjVwk1SPIBtQA3q(nvA4cYYj65q10LCDG2UG67svvz8svCEsbVxQQsZNuO6(KGu7xnuhOkmQhg9HrFOkm6lWvOarakr4egTooWOVapKJvdQzpfm6PaGraG4LRE1SNcgdaIaucx00OHXMZ5ueyQPSuf6tJgMASPfuAnLOWTuf6t5SAs0yZ5CoNZPfuAnLOWT6ALTnJefUrdap2CoNZ5CAbLwtjkClvH(uo1o1uwQc9PrdtpEQBlD1SUap2CoNjYFSe5dJYuPWfPQHqjmwzpfmAraACLkIRavWOQcxr8if1lQWOfqjcNQsHsLPSRRFc9AN1rxyuGeLjaU6d5Kv6d9QoWOfqjCr4qvyuGefufg9DnbaeVC1d9amcaeVC1dJoUaSot1gbUzKO6tJMAN6Olq0Sca6yoUaSot1gbUzKOILuGuc8yoUaSot1gbExRSDkbEmlO0AkrHBPk0NYP2PUap2ybarakHlAAbLwtjkCR(L4z11kBNs8PrdJnNZPfuAnLOWT6ALTtPQTxNCQD2FhBoNZ5CodwNoMANLem9ES5CoNZ5m4o1Xu7SKGP3JnNZ5CoNb7jDm1oljy69yZ5CoNZzWaPJP2zjbtVhBoNZ5CodwZDm1oljy69yZ5CoNZzWaTJP2zjbtVhBoNZ5CodwXDm1oljy69yZ5CoNZzWEUJP2zjbZXMZ5uHg4XsK)ybarakHlAAbLwtjkClViZSRv2oL4tJggBoNZaUYPqRoEpbIUtF3XMZ5CoNt1gbUzKOILuGuo1odcz1NHCSvguRwLlEjfiLgfgaES5CoNZ5ueyAgjkfThbEHBuTrGBgjQyjfiLgMwK)0msu9HRWe4IEDsJQncCZirflPaP0WSAs0yZ5CoNZ5CoNMrIQRv2oLtTtnLv)ScFvRuzw9H8ZEmQ2iWnJevSKcKsdap2CoNZ5CoNZPiW0msuDTY2PCUgx7ejCMvtIgBoNZ5CoNZ5CoNtlO0AkrHB11kBNsvBVozWMrIQRv2oLDm1odyDWtGhBoNZ5CoNZ5mr(JnNZ5CoNjYFS5CotK)yjYFSaGiaLWfnTGsRPefULxZEQUwz7uonAyS5Cod4kNcT6490ZtF3XMZ5CoNtrGPfuAnLOWT6ALTtPQTxNmyrhZQjrJnNZ5CoNZ5CkZcuIMcGhBoNZ5CotK)yZ5CMi)XMZ5uMfOenrchGhlr(JfaebOeUOPfuAnLOWT6ALTnJef(0OHXMZ5ueyAgjQ(W1K0fmKhLw)uAmOqownKYGmmDLtZir1hUMKUGHuiXXf4LfvgdkKJvdPmidZQjrJnNZ5CoNwqP1uIc3QFjEwDTY2Pe3ObGhBoNZ5CoTGsRPefULxKz21kBNsCJgaES5CoNZ5mGRCk0QJ3tGO703DS5CoNZ5CoNt1gbUzKOILuGuo1odcz1NHCSvguRwLlEjfiLgfgaES5CoNZ5CoNthxawNPsqH)u70msuAYCFAKr1gbUzKOILuGuAy6kNbf0yZ5CoNZ5CoNoUaSotH)u7SCrG67nLgvck87)6TfGgJb1zJEQVhBOZEApEC)jidEp1rxdJnNZ5CoNZ5CkcmnJeLI2JaVWnQ2iWnJevSKcKsdtlYFAgjQ(WvycCrVoPr1gbUzKOILuGuAyAr(ZYfbQV3uAu4ByQv7uTrGBgjQ(0OPf5prU1utz1pRWx1kvMvFi)ShJQncCZirflPaP0WSAs0yZ5CoNZ5CoNZ5CQ2iW7ALTt5u70ckTMsu4wEn7P6ALTtPrdap2CoNZ5CoNZ5CoNIat1gbExRSDkNA1orcNz1KOXMZ5CoNZ5CoNZ5CoNZELPTnWJnNZ5CoNZ5CoNZz6GNJnNZ5CoNZ5CoNZ5CoNXS6Nv4RALkZYOAJa3msuXskqk9EQ2iW7ALTtPbGhBoNZ5CoNZ5CoNZ5CoTGsRPefUvxRSDkvT96KbR2iW7ALTtzhtTZawh8e4XMZ5CoNZ5CoNZ5mr(JnNZ5CoNZ5CMi)XMZ5CoNZe5p2CoNjYFSe5dlybJ4PtafXvGkyuvHRiEKI6fvy067lkHlY6yQKcLktjoufwWyjfkvMsOkmwAiucJaaXlx9WiiEAQjTovdSxyeaiE5Q3uPHqjmIx2cehgbrz4evWiaq8YvpmgkLasu1KwNQbg1fgbXttGirb9amcOjb0ggZWjQGrq80uOucirvtADQgOhGXqPeqIQM06unMCdLsB5kHrngmcaeVC1dJbarakHlYOHXMZ5uMfOenljyowI8hBSXgBSXgBmybJcFyun1hYXw5mkuIYzFAem2)e(vd0lkwZ6QzfOafOypbOEgOarXa1ZWaDwZ6cJiq9QsHsLPeh6vDGXMuOSPGEbOAgOkwXkWZarb6QyfOJc8eGcd0zGOay0X16dvHXY6KOQtqvyupOtjGQWcwWiiEAcLHtj0dWyzDsubvHr9GoLaQclybJjcGliQsPWhg1XZavXWiiEAkraCbrvkf(qpaJ(IabeaaDknavHr9GoLaQclybJslGeQcJ6bDkbufwWcgdfouzDsubvHr9GoLaQclybJbSakHlcQcJ6bDkbufwWcgLUojufg1d6ucOkSGfSGrqujfkvMsOkmcINMarIc6bymukbKOQjTovdmQlmcINMcLsajQAsRt1a9amgkLasu1KwNQXKBOuAlxjmQqOfgbrIcgd5y1qkHfSGrCyStTasRobJHHEvhG0jDGfec
+
+--[[
+Author : Quichons, Kutikuti
+Version : 1.0 (06.01.2017)
+Readme :
+	WeakAura to mark scrubber with markers (with unit ID)
+]]--
+
+
+--- Actions\On Init\Custom ---
+
+-- Init
+local ThisUnitID = 104596
+local ThisUnitString;
+local ThisMarker;
+aura_env.Timer = 0;
+
+function aura_env.ResetMarkers ()
+    aura_env.MarkerTable = {
+        [1] = true,
+        [2] = true,
+        [3] = true,
+        [4] = true,
+        [5] = true,
+        [6] = true,
+        [7] = true,
+        [8] = true
+    };
+end
+function aura_env.FreeMarkers ()
+    for i=1, 40 do
+        ThisUnitString = "nameplate"..tostring(i);
+        if UnitExists(ThisUnitString) and UnitIsVisible(ThisUnitString) then
+            UnitMarker = GetRaidTargetIndex(ThisUnitString);
+            if UnitMarker ~= nil then
+                aura_env.MarkerTable[UnitMarker] = false;
+            end
+        end
+    end
+end
+function aura_env.NextMarker ()
+    for i=1, 8 do
+        if aura_env.MarkerTable[i] then
+            return i;
+        end
+    end
+    return nil;
+end
+function aura_env.MarkUnits ()
+    if UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") then
+        aura_env.ResetMarkers();
+        aura_env.FreeMarkers();
+        for i=1, 40 do
+            ThisUnitString = "nameplate"..tostring(i);
+            local guid = UnitGUID(ThisUnitString) or ""
+            local id = tonumber(guid:match("-(%d+)-%x+$"), 10)
+            if UnitExists(ThisUnitString) and UnitIsVisible(ThisUnitString) and tonumber(id) == ThisUnitID and not GetRaidTargetIndex(ThisUnitString) then
+                ThisMarker = aura_env.NextMarker();
+                if ThisMarker == nil then
+                    break;
+                else
+                    SetRaidTarget(ThisUnitString, ThisMarker);
+                    aura_env.MarkerTable[ThisMarker] = false;
+                end
+            end
+        end
+    end
+end
+
+-- Show (Custom)
+function ()
+    if GetTime() > aura_env.Timer then
+        aura_env.MarkUnits();
+        aura_env.Timer = GetTime() + 0.050;
+    end
+end
+
+--- Trigger\Custom Trigger ---
+
+-- Trigger [Status\Every Frame]
+function()
+    return true;
+end
+
